2528e03354e6a3eae8a35ef75bfdb54a67966d7f
Clara Loforti <clara.loforti@gmail.com>
2025-12-15 17:31:18 +0100

    snacks 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,51 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
+
+// CREO ARRAY SQUADRE
+let squadre = [
+    {
+        "nome": "Inter",
+        "punti": 0,
+        "falli": 0
+    },
+    {
+        "nome": "Juve",
+        "punti": 0,
+        "falli": 0
+    },
+    {
+        "nome": "Milan",
+        "punti": 0,
+        "falli": 0
+    },
+    {
+        "nome": "Roma",
+        "punti": 0,
+        "falli": 0
+    }
+];
+
+// Genero numero di punti fatti e falli subiti random
+function numRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+for (let i = 0; i < squadre.length; i++) {
+    squadre[i].punti = numRandom(0, 10);
+    squadre[i].falli = numRandom(0, 10);
+
+    document.getElementById("risultato").innerHTML += ("I falli subiti da " + squadre[i].nome + " sono: " + squadre[i].falli + "<hr>");
+    }
+    
+
+// Creo array che contiene rispettivamente per ogni squadra nome e falli subiti
+let arrayNomeFalli = [];
+for (let i = 0; i < squadre.length; i++) {
+    arrayNomeFalli[i] = {
+    "nome": squadre[i].nome,
+    "falli": squadre[i].falli
+  };
+}
+
+console.log(arrayNomeFalli);
