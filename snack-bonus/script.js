3a9b370dcca8a479acb84b7164c5e21fd6940a6f
Clara Loforti <clara.loforti@gmail.com>
2025-12-15 13:15:53 +0100

    inizializzo repository

---
+++ snack-bonus/script.js
@@ -0,0 +1,11 @@
+/*
+BONUS 
+ 
+Crea 10 oggetti che rappresentano una zucchina.
+Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+NOTE
+NON usate cicli/metodi di JS avanzati (filter, map, o simili), li vedremo a parte e così vi sforzate ad usare la logica.
+
+Buon divertimento... 🚲 ⚽ 🥒
